ecc34b32cf5d09075dc457278f8999fe4ae31f67
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-03-25 20:08:17 -0300

    Import home page coffee paragraph element creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("Hello World!");
+import './style.css';
+import { createHomeCoffeeParagraph } from './home.js';
+
+// Wait for DOM to load
+document.addEventListener('DOMContentLoaded', () => {
+  createHomeCoffeeParagraph();
+});
