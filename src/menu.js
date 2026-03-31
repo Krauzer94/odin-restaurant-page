032662ad9e35d5775b7b06d3877013f071a06372
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-03-31 19:52:10 -0300

    Menu page factory functions

---
+++ src/menu.js
@@ -0,0 +1,56 @@
+const mainElement = document.getElementById('content-menu');
+
+// Image factory module
+function createMenuImage(imageFile, imageAlt) {
+
+  // Image cotainer
+  const menuImageDiv = document.createElement('div');
+  menuImageDiv.className = "coffee-sweets-imgs-mp";
+  mainElement.appendChild(menuImageDiv);
+
+  // Image element
+  const menuImgElement = document.createElement('img');
+  menuImageDiv.appendChild(menuImgElement);
+
+  // Image content
+  menuImgElement.src = imageFile;
+  menuImgElement.alt = imageAlt;
+  menuImageDiv.appendChild(menuImgElement);
+};
+
+// Paragraph factory module
+function createMenuParagraph(
+  titleText,
+  bodyText,
+  smallOption,
+  mediumOption,
+  largeOption,
+) {
+
+  // Headline container
+  const menuParagraphDiv = document.createElement('div');
+  menuParagraphDiv.className = 'headline-paragraph';
+  mainElement.appendChild(menuParagraphDiv);
+
+  // Headline content
+  const menuParagraphH2 = document.createElement('h3');
+  menuParagraphH2.textContent = titleText;
+  menuParagraphDiv.appendChild(menuParagraphH2);
+
+  // Paragraph element
+  const menuParagraphP = document.createElement('p');
+  menuParagraphP.className = 'headline-paragraph-text-mp';
+  menuParagraphP.textContent = bodyText;
+  menuParagraphP.appendChild(document.createElement("br"));
+  menuParagraphP.appendChild(document.createElement("br"));
+
+  // Menu options: small, medium, large
+  menuParagraphP.append(`- Small ${smallOption}`);
+  menuParagraphP.appendChild(document.createElement("br"));
+  menuParagraphP.append(`- Medium ${mediumOption}`);
+  menuParagraphP.appendChild(document.createElement("br"));
+  menuParagraphP.append(`- Large ${largeOption}`);
+  menuParagraphDiv.appendChild(menuParagraphP);
+};
+
+export { createMenuImage, createMenuParagraph };
